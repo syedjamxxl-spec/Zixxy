aa398acccd412acecd71a3f90d81212fada6f6e1
syedjamxxl-spec <syedjamxxl@gmail.com>
2026-08-30 23:21:30 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Zixxy 🎉</h1>
+      <p>If you can see this, your app is live!</p>
+    </div>
+  );
+}
